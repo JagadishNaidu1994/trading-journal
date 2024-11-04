f1ac62bd473fd699ad83871e7aab79e465605ba6
Bondada Jagadish <50915479+JagadishNaidu1994@users.noreply.github.com>
2024-11-04 22:22:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Define your CSV URL here
-const csvUrl = "https://raw.githubusercontent.com/username/repository/main/filename.csv";
+const csvUrl = "https://raw.githubusercontent.com/JagadishNaidu1994/trading-journal/refs/heads/main/trades.csv";
 
 // Function to sync data from online CSV
 async function syncData() {
